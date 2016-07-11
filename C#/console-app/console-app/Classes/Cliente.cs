1e03313edc5bda305d676411bd9265b456c2dd3a
Didox Aparecido <Didox Aparecido>
2016-07-10 22:22:05 -0300

    polimorfismo

--- C#/console-app/console-app/Classes/Cliente.cs
+++ C#/console-app/console-app/Classes/Cliente.cs
@@ -45,64 +45,39 @@ namespace Classes
 
         private string sobrenome = "Santos";
 
-        public void Gravar()
+        public virtual void Gravar()
         {
-            if(this.GetType() == typeof(Cliente))
+            var clientes = Cliente.LerClientes();
+            clientes.Add(this);
+            if (File.Exists(caminhoBase()))
             {
-                var clientes = Cliente.LerClientes();
-                clientes.Add(this);
-                if (File.Exists(caminhoBaseClientes()))
+                StreamWriter r = new StreamWriter(caminhoBase());
+                r.WriteLine("nome;telefone;cpf;");
+                foreach (Cliente c in clientes)
                 {
-                    StreamWriter r = new StreamWriter(caminhoBaseClientes());
-                    r.WriteLine("nome;telefone;cpf;");
-                    foreach (Cliente c in clientes)
-                    {
-                        var linha = c.Nome + ";" + c.Telefone + ";" + c.CPF + ";";
-                        r.WriteLine(linha);
-                    }
-                    r.Close();
+                    var linha = c.Nome + ";" + c.Telefone + ";" + c.CPF + ";";
+                    r.WriteLine(linha);
                 }
-            }
-            else
-            {
-                var usuario = Usuario.LerUsuarios();
-                Usuario u = new Usuario(this.Nome, this.Telefone, this.CPF);
-                usuario.Add(u);
-                if (File.Exists(caminhoBaseUsuarios()))
-                {
-                    StreamWriter r = new StreamWriter(caminhoBaseUsuarios());
-                    r.WriteLine("nome;telefone;cpf;");
-                    foreach (Usuario c in usuario)
-                    {
-                        var linha = c.Nome + ";" + c.Telefone + ";" + c.CPF + ";";
-                        r.WriteLine(linha);
-                    }
-                    r.Close();
-                }
+                r.Close();
             }
         }
 
-        private void Olhar()
+        public virtual void Olhar()
         {
             Console.WriteLine("O cliente " + this.Nome + " " + this.sobrenome + " está olhando para mim");
         }
 
-        private static string caminhoBaseClientes()
+        private static string caminhoBase()
         {
             return ConfigurationManager.AppSettings["BaseDeClientes"];
-        }
-
-        private static string caminhoBaseUsuarios()
-        {
-            return ConfigurationManager.AppSettings["BaseDeUsuarios"];
         }
 
         public static List<Cliente> LerClientes()
         {
             var clientes = new List<Cliente>();
-            if (File.Exists(caminhoBaseClientes()))
+            if (File.Exists(caminhoBase()))
             {
-                using (StreamReader arquivo = File.OpenText(caminhoBaseClientes()))
+                using (StreamReader arquivo = File.OpenText(caminhoBase()))
                 {
                     string linha;
                     int i = 0;
@@ -118,29 +93,6 @@ namespace Classes
                 }
             }
             return clientes;
-        }
-
-        public static List<Usuario> LerUsuarios()
-        {
-            var usuarios = new List<Usuario>();
-            if (File.Exists(caminhoBaseUsuarios()))
-            {
-                using (StreamReader arquivo = File.OpenText(caminhoBaseUsuarios()))
-                {
-                    string linha;
-                    int i = 0;
-                    while ((linha = arquivo.ReadLine()) != null)
-                    {
-                        i++;
-                        if (i == 1) continue;
-                        var usuarioArquivo = linha.Split(';');
-
-                        var usuario = new Usuario(usuarioArquivo[0], usuarioArquivo[1], usuarioArquivo[2]);
-                        usuarios.Add(usuario);
-                    }
-                }
-            }
-            return usuarios;
         }
     }
 }
--- C#/console-app/console-app/Program.cs
+++ C#/console-app/console-app/Program.cs
@@ -15,10 +15,10 @@ namespace console_app
         static void Main(string[] args)
         {
             //Menu.Criar();
-
+            /*
             Console.WriteLine("===========Cadastro de cliente=====================");
             Cliente c = new Cliente();
-            c.Nome = "Danilo 2";
+            c.Nome = "Cliente";
             c.Telefone = "23223";
             c.CPF = "3333";
             c.Gravar();
@@ -28,21 +28,21 @@ namespace console_app
                 Console.WriteLine(cl.Telefone);
                 Console.WriteLine(cl.CPF);
                 Console.WriteLine("================================");
-            }
+            }*/
 
             Console.WriteLine("===========Cadastro de usuario=====================");
             Usuario u = new Usuario();
-            u.Nome = "Danilo 2";
+            u.Nome = "Usuario";
             u.Telefone = "23223";
             u.CPF = "3333";
-            u.Gravar();
-            foreach(Usuario us in Usuario.LerUsuarios())
+            u.Olhar();
+            /*foreach(Usuario us in Usuario.LerUsuarios())
             {
                 Console.WriteLine(us.Nome);
                 Console.WriteLine(us.Telefone);
                 Console.WriteLine(us.CPF);
                 Console.WriteLine("================================");
-            }
+            }*/
         }
     }
 }
