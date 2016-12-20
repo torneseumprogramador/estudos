e0a1adb86d150c1466bc799639bdcf7545d0062d
Didox <dansantos@abril.com.br>
2016-12-20 10:28:34 -0200

    interface modelo

--- nodejs/cms-interface/app/models/usuario.js
+++ nodejs/cms-interface/app/models/usuario.js
@@ -1,3 +1,6 @@
+var request = require("request");
+var host = "http://localhost:3000";
+
 var Usuario = function(usuario){
   if(usuario !== undefined){
     this.id = usuario.id;
@@ -15,8 +18,45 @@ var Usuario = function(usuario){
   }
 
   this.salvar = function(callback){
-    
+    var usuario = this;
+    request.head(host + "/usuarios.json", function(){
+      token = this.response.headers.auth_token;
+      request.post({
+        url: host + "/usuarios.json", 
+        headers: {'auth_token': token}, 
+        form: usuario
+      }, 
+      function(error, response, body) {
+        if(response.statusCode == 201){
+          callback({
+            erro: false
+          });
+        }
+        else{
+          var json = JSON.parse(response.body);
+          callback({
+            erro: true,
+            mensagem: json.erro
+          });
+        }
+      });
+    });
   };
+};
+
+Usuario.todos = function(callback){
+  request.get(host + "/usuarios.json", function(error, response, body) {
+    var json = JSON.parse(response.body);
+    if(response.statusCode == 200){
+      callback(json);
+    }
+    else{
+      callback({
+        erro: true,
+        mensagem: json.erro
+      });
+    }
+  });
 };
 
 module.exports = Usuario;
