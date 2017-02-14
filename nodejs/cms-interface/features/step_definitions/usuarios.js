e7874bc5832ddf68cec8c722edb09dabab9cf94a
Didox <dansantos@abril.com.br>
2017-02-14 08:19:36 -0200

    aula BDD node

--- nodejs/cms-interface/features/step_definitions/usuarios.js
+++ nodejs/cms-interface/features/step_definitions/usuarios.js
@@ -3,20 +3,51 @@ var {defineSupportCode} = require('cucumber');
 
 defineSupportCode(function({Given, When, Then}) {
 
+  var tituloNovoCadastro = undefined;
+
   Given('that I am in the registration page {arg1:stringInDoubleQuotes}', function (url, callback) {
+    this.driver.sleep(800);
     this.driver.get(url);
     callback();
   });
 
   Given('click on the new button', function (callback) {
+    this.driver.sleep(800);
     this.driver.findElement({css:'#novo-registro'}).then(function(element) {
       element.click();
+      callback();
+    });
+  });
+  
+  Given('fill in the field {arg1:stringInDoubleQuotes} with {arg2:stringInDoubleQuotes}', function (campo, valor, callback) {
+    this.driver.sleep(800);
+    if(campo === "nome"){
+      tituloNovoCadastro = valor + " - " + new Date().toString();
+      valor = tituloNovoCadastro;
+    }
+
+    this.driver.findElement({css:'input[name="' + campo + '"]'}).sendKeys(valor);
+    callback();
+  });
+
+  Given('click the save button', function (callback) {
+    this.driver.sleep(800);
+    this.driver.findElement({css:'input[type="submit"]'}).then(function(element) {
+      element.click();
+      callback();
     });
   });
 
-  Given('fill in the field name {arg1:stringInDoubleQuotes}', function (nome, callback) {
-    this.driver.findElement({css:'#nome'}).sendKeys(nome);
-    callback();
+  Then('I must see the new register', function (callback) {
+    this.driver.sleep(800);
+    this.driver.findElement({css:'tr:last-child td'}).then(function(element) {
+      element.getText().then(function(textValue){
+        if(tituloNovoCadastro !== textValue){
+          throw "Register not found, register should be = (" + tituloNovoCadastro + "), and return = (" + textValue + ") ";
+        }
+        callback();
+      });
+    });
   });
 
 });
--- nodejs/cms-interface/features/usuarios.feature
+++ nodejs/cms-interface/features/usuarios.feature
@@ -1,12 +1,12 @@
 Feature: Test or register users
   Need to register a user
   
-  Scenario: Register a user
+  Scenario: Register a new user
 	Given that I am in the registration page "http://local.com.br:3001/usuarios"
 	And click on the new button
-	And fill in the field name "BDD test"
-	And fill in the login field "bdd"
-	And fill in the password field "bdd"
-	And fill in the email field "bdd@teste.com"
+	And fill in the field "nome" with "BDD test"
+	And fill in the field "login" with "bdd"
+	And fill in the field "senha" with "bdd"
+	And fill in the field "email" with "bdd@teste.com"
 	And click the save button
-	Then I must see the message of success.
+	Then I must see the new register
