bf7ad68386364f9eb6e027ddb1a27a2117c92794
Didox <dansantos@abril.com.br>
2017-04-19 09:29:38 -0300

    grunt

---
+++ nodejs/cms-interface/Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    uglify: {
+      build: {
+        src: 'public/javascripts/*.js',
+        dest: 'public/javascripts/build/aplicacao.min.js'
+      }
+    },
+    cssmin:{
+      target: {
+        files: {
+          'public/stylesheets/build/aplicacao.min.css': ['public/stylesheets/*.css']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.registerTask('default', ['uglify', 'cssmin']);
+};
