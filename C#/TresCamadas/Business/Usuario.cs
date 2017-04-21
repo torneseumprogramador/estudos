f9b4189c78ce57311976ca820ac0697c9b4b4297
Didox Aparecido <Didox Aparecido>
2017-04-21 09:43:20 -0300

    3 camadas c#

---
+++ C#/TresCamadas/Business/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Database;
+
+namespace Business
+{
+    public class Usuario : Base
+    {
+        public Usuario(string nome, string telefone, string cpf)
+        {
+            this.Nome = nome;
+            this.Telefone = telefone;
+            this.CPF = cpf;
+        }
+
+        public Usuario() { }
+    }
+}
---
+++ C#/TresCamadas/Database/IPessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public interface IPessoa
+    {
+        void SetNome(string nome);
+        void SetTelefone(string telefone);
+        void SetCPF(string cpf);
+        void Gravar();
+    }
+}
