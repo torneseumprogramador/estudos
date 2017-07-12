bc5c5ff63de170d55b6d322f15320ca8313fa932
Didox Aparecido <Didox Aparecido>
2017-07-12 09:00:49 -0300

    criar tabela

---
+++ C#/TresCamadasPersistentesGenerics/Business/Carro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Database;
+
+namespace Business
+{
+    public class Carro : Base
+    {
+        [OpcoesBase(UsarNoBancoDeDados = true, UsarParaBuscar = true, ChavePrimaria = true)]
+        public int ID { get; set; }
+
+        [OpcoesBase(UsarNoBancoDeDados = true)]
+        public string Nome { get; set; }
+
+        [OpcoesBase(UsarNoBancoDeDados = true)]
+        public string Marca { get; set; }
+
+        [OpcoesBase(UsarNoBancoDeDados = true)]
+        public int Quantidade { get; set; }
+
+        [OpcoesBase(UsarNoBancoDeDados = true)]
+        public double Valor { get; set; }
+
+    }
+}
